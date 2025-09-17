dade0a7076731d4ac40ce8a8be4187b1d8ce70a5
bigspace39 <bigspace39@gmail.com>
2025-09-18 00:05:06 +0200

    Started working on donut chart, imported chartjs and made base UI for it.

---
+++ widgets/donut-chart.js
@@ -0,0 +1,51 @@
+class DonutChart extends Widget {
+    static displayName = "Donut Chart";
+
+    static {
+        possibleWidgets.push(this);
+    }
+
+    canvas;
+    label;
+    chart;
+    data = {
+        labels: [
+            'Red',
+            'Blue',
+            'Yellow'
+        ],
+        datasets: [{
+            data: [300, 50, 100],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+        }]
+    };
+
+    constructor(dashboardElement, classIndex, dashboardId, widgetId = -1, transform = null) {
+        super(dashboardElement, classIndex, dashboardId, widgetId, transform);
+        this.label = createElement("h1", this.contentDiv, "donut-label");
+        this.label.innerText = "Label";
+        this.canvas = createElement("canvas", this.contentDiv, "donut-chart");
+        this.contentDiv.style.width = "200px";
+        this.contentDiv.style.height = "250px";
+        const config = {
+            type: 'doughnut',
+            data: this.data,
+        };
+
+        this.chart = new Chart(
+            this.canvas,
+            config
+        );
+    }
+
+    async update() {
+        this.data.datasets[0].data[0]++;
+        this.chart.data = this.data;
+        this.chart.update();
+    }
+}
--- widgets/widget-imports.js
+++ widgets/widget-imports.js
@@ -1,1 +1,2 @@
 import "./month-calendar.js";
+import "./donut-chart.js"
