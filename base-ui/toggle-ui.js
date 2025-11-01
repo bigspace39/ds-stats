97dd5889ee76b3c126b9b6c9b1b44c069050b935
bigspace39 <bigspace39@gmail.com>
2025-11-01 18:33:37 +0100

    Added toggle UI to base ui.

---
+++ base-ui/toggle-ui.js
@@ -0,0 +1,31 @@
+class ToggleUI {
+    label;
+    input;
+    span;
+    onToggle = new Delegate();
+
+    constructor(parentElement, toggled = false) {
+        this.label = createElement("label", parentElement, "toggle");
+        this.input = createElement("input", this.label);
+        this.input.type = "checkbox";
+        this.span = createElement("span", this.label, "toggle-slider");
+        this.setToggled(toggled);
+
+        this.input.addEventListener("click", function() {
+            this.toggleUI.onToggle.broadcast(this.toggleUI, this.checked);
+        });
+        this.input.toggleUI = this;
+    }
+
+    setToggled(toggled) {
+        if (this.isToggled() == toggled)
+            return;
+
+        this.input.checked = toggled;
+        this.onToggle.broadcast(this, toggled);
+    }
+
+    isToggled() {
+        return this.input.checked;
+    }
+}
